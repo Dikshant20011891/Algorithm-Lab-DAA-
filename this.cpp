7f8f0e5454191168b0c399385e5379c671b0791d
Dikshant20011891 <91108862+Dikshant20011891@users.noreply.github.com>
2022-05-28 15:42:07 +0530

    Update this.cpp

--- this.cpp
+++ this.cpp
@@ -1,29 +1,44 @@
 /******************************************************************************
-
 Welcome to GDB Online.
 GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
 C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
-
 *******************************************************************************/
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
-bool comp(vec[0],vec[1])
+static int a = 1;
+bool comp(pair<pair<int,int>,int> &p1,pair<pair<int,int>,int> &p2)
 {
-    
+    return (p1.second < p2.second);
+}
+
+void checkvisited(pair<int,int> p1, int parent[])
+{
+    if((parent[p1.first] == -1 && parent[p1.second] == -1) || (parent[p1.first] == -1 && parent[p1.second] == 1) ||
+    (parent[p1.first] == 1 && parent[p1.second] == -1))
+    {
+        cout<<a<<" Edge"<<p1.first<<" "<<p1.second<<endl;
+        a++;
+        parent[p1.first] = 1;
+        parent[p1.second] == -1;
+    }
 }
 void kruskal(vector<pair<pair<int,int>,int> > vec,int v, int e)
 {
     int parent[v];
-    int visited[v] = {0};
     
     for(int i=0;i<v;i++)
     {
         parent[i] = -1;
     }
     sort(vec.begin(),vec.end(),comp);
+    
+    for(int i=0;i<e;i++)
+    {
+        checkvisited(vec[i].first,parent);
+    }
 }
 
 int main()
