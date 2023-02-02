c16d567d7e94a75756efae3c641401da1eac7b77
Dikshant Joshi <91108862+Dikshant20011891@users.noreply.github.com>
2023-02-02 11:34:44 +0530

    Update Longest_Comman_Subsquence.cpp

--- Dynamic Programming - DP/Longest_Comman_Subsquence.cpp
+++ Dynamic Programming - DP/Longest_Comman_Subsquence.cpp
@@ -1,3 +1,8 @@
+// Given two sequences, find the length of the longest subsequence present in both of them.
+// LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3. 
+// LCS for input Sequences “AGGTAB” and “GXTXAYB” is “GTAB” of length 4.
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
